79e9dd857a429c26d8655e5d3593eea1c9fcf1df
alfonsoaranzazu <alfonso@viruett.com>
2019-04-11 23:06:47 -0700

    use header html tag

--- hacker-news-search/src/sections/Header.js
+++ hacker-news-search/src/sections/Header.js
@@ -5,12 +5,14 @@ import './Header.css';
 export default class Header extends Component {
     render() {
         return (
-            <div className="Header">
-                <img className="logo" src={logo}></img>
-                <div className="logo-text">
-                    <p>Search Hacker News</p>
-                </div>
-                <input className="search" type="search" placeholder="Search..."></input>
+            <div>
+                <header className="Header">
+                    <img className="logo" src={logo}></img>
+                    <div className="logo-text">
+                        <p>Search Hacker News</p>
+                    </div>
+                    <input className="search" type="search" placeholder="Search..."></input>
+                </header>
             </div>
         )
     }
